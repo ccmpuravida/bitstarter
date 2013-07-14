9a5ded4df38a32fda0620943ce56cbaa81758b64
Christian Calvo <ccmpuravida@gmail.com>
2013-07-14 22:00:58 +0000

    modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var stext = fs.readFileSync("index.html");
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(stext);
 });
 
 var port = process.env.PORT || 5000;
